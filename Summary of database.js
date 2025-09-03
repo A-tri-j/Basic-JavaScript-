7f76a1bb149a2e2e9aa7572338d14cce7c1aef60
Atrij Ghosh <atrijghosh07@gmail.com>
2025-09-03 20:57:10 +0530

    datatype summary

---
+++ Summary of database.js
@@ -0,0 +1,33 @@
+// primitive 
+
+// 7 type : string ,number, bollean ,null,undefined,symbol,bigInt
+const  score =100
+const scoreValue =100.34
+
+const isLoggedIn=false
+const outsideTemp =null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId =symbol('123')
+
+console.log (id ===anotherId);
+
+const bigNumber = 3445785420565244444n
+
+
+// Reference (Non primitive)
+
+//Array,objects,Functions
+
+const  heros =["shaktiam","naagraj","doga"];
+let myobj={
+     name : "atrij";
+     age:20;
+}
+
+
+const myfunstion=function(){
+    console.log("hello world");
+}
+

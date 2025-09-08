3d345afcf9cbe98a9c694329e43fb0b6ab09d60f
Atrij Ghosh <atrijghosh07@gmail.com>
2025-09-08 18:55:24 +0530

    Create data and timmming.js

---
+++ data and timmming.js
@@ -0,0 +1,16 @@
+// Dates 
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleDateString())
+
+
+let myCreateDate =new Date (2025, 8, 8)
+console.log(myCreateDate.toDateString());
+
+
+let CreateDate =new Date ("2025-08-08")
+console.log(CreateDate.toDateString());
+
+let mytimeStemp
